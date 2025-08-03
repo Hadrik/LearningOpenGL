a2262b1f0a661007d5e2405e3c0d6dafb7e0c477
Richard Trávníček <89924371+Hadrik@users.noreply.github.com>
2025-08-03 20:32:57 +0200

    Ex3

--- main.cpp
+++ main.cpp
@@ -68,47 +68,54 @@ int main() {
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
     unsigned int vertex_shader = compile_shader(GL_VERTEX_SHADER, vert_sh_src);
-    const char *frag_sh_src = "#version 330 core\n"
+    const char *frag_sh1_src = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main() {\n"
     "   FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
     "}\0";
-    unsigned int fragment_shader = compile_shader(GL_FRAGMENT_SHADER, frag_sh_src);
-    unsigned int shader_program = link_shader_program(vertex_shader, fragment_shader);
+    unsigned int fragment_shader_1 = compile_shader(GL_FRAGMENT_SHADER, frag_sh1_src);
+    unsigned int shader_program_1 = link_shader_program(vertex_shader, fragment_shader_1);
+    const char* frag_sh2_src = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "   FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n"
+    "}\0";
+    unsigned int fragment_shader_2 = compile_shader(GL_FRAGMENT_SHADER, frag_sh2_src);
+    unsigned int shader_program_2 = link_shader_program(vertex_shader, fragment_shader_2);
     glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+    glDeleteShader(fragment_shader_1);
+    glDeleteShader(fragment_shader_2);
 
 
-    float verts[] = {
-        0.5f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f,
+    float verts1[] = {
+        -1.0, -1.0, 0.0,
+        0.0, -1.0, 0.0,
+        -0.5, 0.0, 0.0,
     };
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
+    float verts2[] = {
+        0.0, 0.0, 0.0,
+        1.0, 0.0, 0.0,
+        0.5, 1.0, 0.0,
     };
 
-    unsigned int vao, vbo, ebo;
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
-    glBindVertexArray(vao);
+    unsigned int vaos[2], vbos[2];
+    glGenVertexArrays(2, vaos);
+    glGenBuffers(2, vbos);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), &verts, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
-
+    glBindVertexArray(vaos[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts1), &verts1, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    glBindVertexArray(vaos[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts2), &verts2, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
@@ -116,18 +123,22 @@ int main() {
         glClearColor(.2, .3, .3, 1.);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
-        glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glUseProgram(shader_program_1);
+        glBindVertexArray(vaos[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUseProgram(shader_program_2);
+        glBindVertexArray(vaos[1]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &ebo);
-    glDeleteProgram(shader_program);
+    glDeleteVertexArrays(2, vaos);
+    glDeleteBuffers(2, vbos);
+    glDeleteProgram(shader_program_1);
+    glDeleteProgram(shader_program_2);
     glfwTerminate();
     return 0;
 }
